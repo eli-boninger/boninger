921148e47853ecb08e920933ec14a9d5c46a4d5c
Eli Boninger <eboninger@gmail.com>
2025-04-01 10:54:15 -0400

    lazy load iframes

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,5 +1,4 @@
 import { Divider, Link, Typography } from "@mui/material";
-import React from "react";
 import { BasePage } from "./BasePage";
 import { css } from "@emotion/react";
 
@@ -27,6 +26,7 @@ export const Music = () => (
       src="https://open.spotify.com/embed/artist/64xK0wJW0iMyfFpeeQ9MpC?utm_source=generator"
       allowFullScreen
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
     />
     <Divider color="#B48291" />
     <Typography style={{ marginTop: "2rem" }} sx={{
@@ -43,6 +43,7 @@ export const Music = () => (
       css={iframeStyles}
       src="https://bandcamp.com/EmbeddedPlayer/album=304322865/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/"
       seamless
+      loading="lazy"
     >
       <a href="https://waemel.bandcamp.com/album/wae-mels-pop-hits">
         Wae Mel&#39;s Pop Hits! by Wae Mel
@@ -61,6 +62,7 @@ export const Music = () => (
       css={iframeStyles}
       src="https://bandcamp.com/EmbeddedPlayer/track=2118083282/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/"
       seamless
+      loading="lazy"
     >
       <a href="https://thewombbats.bandcamp.com/track/shapeshifter">
         Shapeshifter by The Womb Bats
@@ -83,6 +85,7 @@ export const Music = () => (
       css={iframeStyles}
       height="166"
       allow="autoplay"
+      loading="lazy"
       src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/311824266&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
     />
   </BasePage>
