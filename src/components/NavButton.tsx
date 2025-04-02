2115dafcba68f97f6e9dd422a126734f43f60c65
Eli Boninger <eboninger@gmail.com>
2025-04-02 17:50:39 -0400

    fix linking bug

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -2,7 +2,7 @@ import { Button, ButtonProps } from "@mui/material";
 import { Link, LinkProps } from "react-router-dom";
 
 export const NavButton = (props: LinkProps & ButtonProps) => {
-  const { role, variant, to, ...otherProps } = props;
+  const { role, variant, ...otherProps } = props;
   return (
     <Button component={Link} variant="text" {...otherProps} />
   );
